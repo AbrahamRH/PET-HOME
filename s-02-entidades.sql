f64129f9e1444f6d4d9d9d5a0074cfb68fae67dc
AbrahamRH <abraham.esc.tra@gmail.com>
2021-12-21 20:29:31 -0600

    Se arreglo la indentación del archivo s-02

--- s-02-entidades.sql
+++ s-02-entidades.sql
@@ -15,15 +15,15 @@
 create table centro_operativo
 (
 	centro_operativo_id number(10,0)  not null,
-	nombre 							varchar2(40)  not null,
-	direccion 					varchar2(100) not null,
-	latitud 						numeric(4,1)  not null,
-	longitud 						numeric(4,1)  not null,
-	codigo 							varchar2(5)   not null,
-	es_refugio 					number(1,0)   not null,
-	es_clinica 					number(1,0)   not null,
-	es_oficina 					number(1,0)   not null,
-	gerente_id 					number(10,0)  not null,
+	direccion 			varchar2(100) not null,
+	nombre 				varchar2(40)  not null,
+	latitud 			numeric(4,1)  not null,
+	longitud 			numeric(4,1)  not null,
+	codigo 				varchar2(5)   not null,
+	es_refugio 			number(1,0)   not null,
+	es_clinica 			number(1,0)   not null,
+	es_oficina 			number(1,0)   not null,
+	gerente_id 			number(10,0)  not null,
 
 	constraint centro_operativo_pk primary key(centro_operativo_id),
 	constraint centro_operativo_uk unique(codigo),
@@ -39,15 +39,15 @@ create table centro_operativo
 -- EMPLEADO
 create table empleado 
 (
-	empleado_id 				number(10,0) not null,
-	nombre 							varchar2(40) not null,
+	empleado_id 		number(10,0) not null,
+	nombre 				varchar2(40) not null,
 	apellido_paterno  	varchar2(40) not null,
-	apellido_materno 		varchar2(40) not null,
-	curp 								varchar2(18) not null,
-	fecha_ingreso 			date 				 not null,
-	email 				 			varchar2(40) not null,
-	sueldo 				 		  number(8,2)  not null,
-	es_gerente 			  	number(1,0)  not null,
+	apellido_materno 	varchar2(40) not null,
+	curp 				varchar2(18) not null,
+	fecha_ingreso 		date 		 not null,
+	email 				varchar2(40) not null,
+	sueldo 				number(8,2)  not null,
+	es_gerente 		  	number(1,0)  not null,
 	es_administrativo   number(1,0)  not null,
 	es_veterinario 	  	number(1,0)  not null,
 	grado_academico_id  number(10,0) not null,
@@ -61,12 +61,12 @@ create table empleado
 -- GRADO_ACADEMICO
 create table grado_academico(
 	grado_academico_id number(10,0) not null,
-	titulo 						 varchar2(40) not null,
-	fecha_titulacion   date  				not null,
+	titulo 			   varchar2(40) not null,
+	fecha_titulacion   date  		not null,
 	cedula_profesional varchar2(20) not null,
-	empleado_id 			 number(10,0) not null,
+	empleado_id 	   number(10,0) not null,
 	constraint grado_academico_pk primary key(grado_academico_id),
 	constraint grado_academico_cedula_profesional_uk unique(cedula_profesional)
 	constraint grado_academico_empleado_id_fk foreign key(empleado_id)
 		references empleado(empleado_id)
-):
+);
