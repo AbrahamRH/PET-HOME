5e29ca1303e16db33a92a59767a904212df7d197
AbrahamRH <abraham.esc.tra@gmail.com>
2021-12-27 14:13:20 -0600

    Se creo el archivo s-08-vistas.sql y se le agregaron 3 vistas

---
+++ s-08-vistas.sql
@@ -0,0 +1,35 @@
+--@autor:           Abraham Ramírez Hernández
+--@Fecha creación:  2021-12-27
+--@Descripción:     (s-08-vistas) - Script para la creación de vistas
+
+
+-- V_MASCOTAS_EN_ADOPCION
+-- Vista creada para ver que mascotas pueden solicitar para adopcion
+create or replace view v_mascotas_en_adopcion
+(
+	nombre, fecha_nacimiento, nombre_tipo, estatus
+) as select m.nombre, m.fecha_nacimiento, t.nombre_tipo, em.descripcion
+from mascota m
+join tipo_mascota t 
+	on m.tipo_mascota_id = t.tipo_mascota_id
+join estatus_mascota em 
+	on em.estatus_mascota_id = m.estatus_mascota_id
+where em.descripcion in ('DISPONIBLE_PARA_ADOPCION', 'SOLICITADA_PARA_ADOPCION');
+
+
+-- V_CLIENTE
+-- Vista para verficar los datos del cliente sin su contraseña de usuarios
+create or replace view v_cliente 
+(
+	cliente_id, nombre, apellido_paterno, apellido_materno, ocupacion
+)as select cliente_id, nombre, apellido_paterno, apellido_materno, ocupacion
+from cliente;
+
+-- V_LISTA_GERENTES
+create or replace view v_lista_gerentes
+(
+	empleado_id, nombre, apellido_paterno, apellido_materno,
+	centro_operativo_id, nombre_centro, direccion
+) as select e.empleado_id, e.nombre, e.apellido_paterno, e.apellido_materno,
+	c.centro_operativo_id, c.nombre, c.direccion
+where e.es_gerente = 1;
