187a6f4f0e42de36bf5ce5e47e60b453d03d4f9c
Hugo Juárez <aom921@hotmail.com>
2021-12-28 12:42:47 -0600

    Esqueleto para los scripts 5,6 y 7

---
+++ s-05-secuencias.sql
@@ -0,0 +1,6 @@
+--@autor:           Juárez Pérez Hugo
+--@Fecha creación:  2021-12-28
+--@Descripción:     Script para realizar la carga inicial todas las tablas de la base de datos.ABORT
+
+--TODO:
+--Llenar con datos de mockaroo xd
