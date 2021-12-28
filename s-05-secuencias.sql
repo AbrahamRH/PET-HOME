e90ae73d3d90e496833748cf594dec59725ee113
AbrahamRH <abraham.esc.tra@gmail.com>
2021-12-28 13:57:07 -0600

    Se agragaron las secuencias de nuestro modelo

--- s-05-secuencias.sql
+++ s-05-secuencias.sql
@@ -1,6 +1,116 @@
---@autor:           Juárez Pérez Hugo
+--@autor:           Abraham Ramírez Hernández
 --@Fecha creación:  2021-12-28
---@Descripción:     Script para realizar la carga inicial todas las tablas de la base de datos.ABORT
+--@Descripción:     (s-05-secuencias) - Script creado para la creacion de secuencias
 
---TODO:
---Llenar con datos de mockaroo xd
+create sequence empleado_seq
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
+
+create sequence mascota_seq
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
+
+create sequence grado_academico_seq
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
+
+create sequence centro_operativo_seq
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
+
+create sequence direccion_web_seq
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
+
+create sequence tipo_mascota_seq
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
+
+create sequence revision_mascota_seq
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
+
+create sequence revision_adopcion_seq
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
+
+create sequence estatus_mascota_seq
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
+
+create sequence historico_estatus_mascota_seq
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
+
+create sequence origen_web
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
+
+create sequence adopcion_seq
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
+
+
+create sequence cliente_seq
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
+
+create sequence donativo_seq
+	start with 100 
+	increment by 1
+	nomaxvalue 
+	nominvalue
+	nocycle
+	cache 5;
