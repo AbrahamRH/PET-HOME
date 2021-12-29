4944df3b9ca2624efde197a49e7b9bc9e5df2fb8
AbrahamRH <abraham.esc.tra@gmail.com>
2021-12-29 15:28:47 -0600

    Se modifico el script de índices, se cambiarion los nombre de los indices, se eliminaron los índices de las pk de su misma tabla y se añadieron nuevos índices

--- s-06-indices.sql
+++ s-06-indices.sql
@@ -1,33 +1,30 @@
---@autor:           Juárez Pérez Hugo
+--@autor:           Juárez Pérez Hugo, Ramírez Hernández Abraham
 --@Fecha creación:  2021-12-28
 --@Descripción:     Script para realizar la carga inicial todas las tablas de la base de datos.ABORT
-
---TODO:
---Revisar su funcionamiento
 
 --Nos conectamos al usuario administrador
 connect rj_proy_admin/rj_admin
 
-
-
 --Indices relacionados a los empleados
-create index indice_empleado_id on empleado(empleado_id);
-
-create index indice_empleado on empleado(upper(nombre,ap_paterno,ap_materno));
-
+create index empleado_nombre_completo_ix on empleado(upper(nombre,ap_paterno,ap_materno));
+create unique index empleado_curp_uix on empleado(curp);
 
 --Indices relacionadas a los centros operativos (?)
-create index indice_CO_id on centro_operativo(centro_operativo_id);
-
-create index indice_CO on centro_operativo(nombre);
-
-
+create index centro_operativo_nombre_ix on centro_operativo(nombre);
+create unique index centro_operativo_codigo_uix centro_operativo(codigo_unico);
 
 --Indices relacionados a las mascotas
-create index indice_mascota on mascota(mascota_id);
+create index mascota_folio_ix on mascota(lower(folio));
+create index mascota_nombre_ix on mascota(lower(nombre));
+create index mascota_cliente_id_ix on mascota(cliente_id);
+create index mascota_estatus_id_ix on mascota(estatus_mascota_id);
 
 --Indices relacionados a los clientes
-create index indice_cliente_id on cliente(cliente_id);
-create index indice_cliente on cliente(nombre, usuario); --(?) Estara bien este????
+create unique index cliente_usuario_uix on cliente(usuario);
 
-
+--Indices relacionados a la adopcion
+create unique index revision_adopcion_revision_id_uix 
+on revision_adopcion(mascota_id, num_revision);
+create index adopcion_mascota_cliente_id_ix on adopcion(mascota_id,cliente_id);
+create index revision_mascota_empleado_mascota_ix 
+on revision_mascota(empleado_id,mascota_id);
