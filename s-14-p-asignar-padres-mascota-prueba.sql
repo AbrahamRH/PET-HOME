833edf0277e43960c71af4e98a042bf4cda5f7bf
Hugo Juárez <aom921@hotmail.com>
2021-12-31 14:16:12 -0600

    Prueba para script 14

---
+++ s-14-p-asignar-padres-mascota-prueba.sql
@@ -0,0 +1,31 @@
+--@autor:           Juárez Pérez Hugo
+--@Fecha creación:  2021-12-31
+--@Descripción:     Script de prueba para el funcionamiento del procedimiento
+--                  del script 13. 
+set serveroutput on
+
+declare
+  c_padre_id number := 1;
+  c_madre_id number := 2;
+  c_hijo_id number := 3;
+
+  begin
+
+  --Llamamos al procedimiento 
+  asignar_padres_mascota(c_padre_id, c_madre_id, c_hijo_id);
+  
+ ---commit al final de las operaciones, todo se ejecutó correctamente.
+  dbms_output.put_line('OK, No hubo errores');
+  commit;
+ 
+  exception 
+  when others then
+  --algo salio mal, se aplica rollback
+  dbms_output.put_line('Algo salio mal, haciendo rollback...');
+  rollback;
+ 
+end;
+/
+
+
+
