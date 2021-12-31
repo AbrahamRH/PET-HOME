3b7c2e1fd9808ee06e44f93e23ceb14ab919f243
Hugo Juárez <aom921@hotmail.com>
2021-12-31 14:17:18 -0600

    Script de prueba para script 17

---
+++ s-18-lob-agregar-imagen-mascota-prueba.sql
@@ -0,0 +1,22 @@
+--@autor:           Juárez Pérez Hugo
+--@Fecha creación:  2021-12-31
+--@Descripción:     Script prueba que los objetos BLOBs tengan contenido
+
+set serveroutput on;
+
+declare
+  v_tamaño_foto
+begin 
+  select dbms_length(foto_mascota) as tamaño_foto 
+  into v_tamaño_foto
+  from revision_mascota
+  where revision_mascota_id = 1;
+
+  if (v_tamaño_foto > 0) THEN
+    dbms_output.putline('OK, blob no vacío');
+  else  
+    dbms_output.putline('Prueba fallida! BLOB esta vacio!!!');
+  end if;
+end;
+/
+
