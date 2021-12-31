538e384eb3b096f5b068208f58a87d935ca61c6a
Hugo Juárez <aom921@hotmail.com>
2021-12-31 14:18:05 -0600

    procedimiento para añadir los padres de una mascota

--- s-13-p-asignar-padres-mascota.sql
+++ s-13-p-asignar-padres-mascota.sql
@@ -2,3 +2,19 @@
 --@Fecha creación:  2021-12-30
 --@Descripción:     Script que añade los padres a una mascota nacida en
 --                  en el refugio.
+
+create or replace procedure asignar_padres_mascota
+(p_padre_id in number, p_madre_id in number, p_hijo_id in number, 
+ p_centro_nacimiento_id in number) is 
+
+BEGIN
+
+  insert into mascota(mascota_madre_id, mascota_padre_id, centro_nacimiento_id) 
+  values( p_madre_id, p_padre_id, p_centro_nacimiento_id )
+  where mascota_id = p_hijo_id;
+
+END;
+/
+
+
+
