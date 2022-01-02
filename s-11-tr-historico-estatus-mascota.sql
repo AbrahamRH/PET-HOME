3637f0aa5c18298d8dd279972c7f51882cb811ca
Hugo Juárez <aom921@hotmail.com>
2022-01-02 11:06:54 -0600

    trigger para el historico de status mascota

---
+++ s-11-tr-historico-estatus-mascota.sql
@@ -0,0 +1,23 @@
+--@autor:           Abraham Ramírez Hernández
+--@Fecha creación:  2022-12-1
+--@Descripción:     (s-11-tr-maximo-adopciones) - Trigger para insertar en el historico de estatus mascota
+create or replace trigger tr_historico_estatus_mascota
+after 
+  insert or
+  update of estatus_mascota_id
+  on mascota
+
+for each row 
+declare 
+  v_mascota_id mascota.mascota_id%type;
+  v_estatus_id mascota.estatus_mascota_id%type;
+begin 
+  v_mascota_id := :new.mascota_id;
+  v_estatus_id := :new.estatus_mascota_id;
+
+  insert into historico_status_mascota(historico_status_mascota_id, fecha_estatus, estatus_mascota_id, mascota_id)
+  values(historico_estatus_mascota_seq.nextval,sysdate,v_estatus_id,v_mascota_id);
+
+end;
+/
+show errors
