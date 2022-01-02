df0e1d978124c125a1b09da9d715779a1d5c7afe
AbrahamRH <abraham.esc.tra@gmail.com>
2022-01-02 14:10:40 -0600

    Se corrigieron las consultas

--- s-10-consultas.sql
+++ s-10-consultas.sql
@@ -8,16 +8,16 @@
 -- tipo de mascota, nombre del refugio y direccion asi como el estatus de la mascota.
 select m.nombre nombre_mascota, trunc((sysdate - m.fecha_nacimiento)/365) edad, 
 	t.nombre_tipo, c.nombre nombre_refugio, c.direccion, em.descripcion
-from mascota m, tipo_mascota t, refugio f, centro c, estatus_mascota em
+from mascota m, tipo_mascota t, refugio r, centro c, estatus_mascota em
 where m.tipo_mascota_id = t.tipo_mascota_id
 and m.estatus_mascota_id  = em.estatus_mascota_id
 and m.centro_operativo_id = r.centro_operativo_id
 and r.centro_operativo_id = c.centro_operativo_id;
 
 
--- Se desea saber la fecha y el monto de la donacion más grande realizada y compararla con el promedio
--- de la tabla externa
-select fecha_donativo, monto_donativo, avg(monto_donativo) 
+-- Se desea saber la fecha y el monto de la donacion más grande realizada y
+-- compararla con el promedio de la tabla externa
+select fecha_donativo, monto_donativo, avg(monto_donativo) promedio
 from donativo_ext
 where monto_donativo = (
 	select max(monto_donativo)
@@ -27,14 +27,14 @@ group by fecha_donativo, monto_donativo;
 
 --Consulta utilizando una vista
 -- Se desea obtener el nombre completo, id y sueldo del gerente con mayor sueldo mensual
-select  g.empleado_id, g.nombre, g.apellido_paterno, g.apellido_materno, q1.sueldo
+select g.empleado_id, g.nombre, g.apellido_paterno, g.apellido_materno, e.sueldo
 from v_lista_gerentes g
-join (
-	select empleado_id, sueldo_mensual
+join empleado e
+  on e.empleado_id = g.empleado_id
+where e.sueldo = (
+	select max(sueldo)
 	from empleado
-	having sueldo_mensual = max(sueldo_mensual)
-) q1
-on q1.empleado_id = g.empleado_id;
+);
 
 --Consulta con un sinonimo y álgebra relacional
 -- Seleccionar todos los datos de los centros operativos menos de los que sean oficinas
@@ -46,7 +46,8 @@ from centro
 where es_oficina = 1;
 
 -- Seleccionar la ultima 
-select rm.empleado_id,m.mascota_id, m.nombre, rm.foto_mascota, rm.fecha_revision, count(*)
+select rm.empleado_id,m.mascota_id, m.nombre, rm.foto_mascota,
+  rm.fecha_revision, count(*)
 from revision_mascota rm
 join mascota m
 on rm.mascota_id = m.mascota_id
