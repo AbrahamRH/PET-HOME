bfe079b5fe7a91f15f8e63dd4651a0b67f1f624c
Hugo Juárez <aom921@hotmail.com>
2022-01-02 20:06:08 -0600

    Se convirtió a compound trigger

--- s-11-tr-ganador-adopcion.sql
+++ s-11-tr-ganador-adopcion.sql
@@ -4,31 +4,37 @@
 --              Al momento de que un cliente gane el proceso de adopción.
 
 create or replace trigger tr_ganador_adopcion
-before
+for
   update of es_ganador
   on adopcion
+compound trigger 
 
-for each row 
-declare 
+--seccion declarativa común
+
+before each row is 
   --Utilizar un cursor para obtener todos aquellos clientes que perdieron el concurso para la mascota 
   --correspondiente
 
   --Marcar a cada uno de ellos con la misma descripción generica por ahora... 
 
   --Asignar el ID del cliente ganador a la mascota correspondiente
-
 begin 
-  
+  update mascota
+  set cliente_id = :new.cliente_id
+  where mascota_id = :new.mascota_id;
+end before each row;
+
+after statement is
+begin
+
 
   update adopcion
   set descripcion_rechazo = 'No cumples con el requirimiento de esta mascota'
   where es_ganador = 0;
 
-  update mascota
-  set cliente_id = :new.cliente_id
-  where mascota_id = :new.mascota_id;
 
 
+end after statement;
 
 
 
