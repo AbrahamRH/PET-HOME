de536a0c57de4d5c30b2702799475f8a5f440dbe
AbrahamRH <abraham.esc.tra@gmail.com>
2022-01-04 16:53:01 -0600

    Se agrego la función 3 para exportar datos pero aun tiene fallos en la compilación

---
+++ s-15-f3-exporta-mascota-csv-prueba.sql
@@ -0,0 +1,34 @@
+--@autor:           Abraham Ramírez Hernández
+--@Fecha creación:  2022-01-04
+--@Descripción:     (s-15-f3-exporta-mascota-csv-prueba) - Prueba de la funcion f3
+
+
+prompt ====================================================
+prompt
+prompt Script de prueva para la función exporta_mascota_csv
+prompt
+prompt ====================================================
+
+
+!mkdir -p /tmp/archivos
+!chmod 777 /tmp/archivos
+!touch /tmp/archivos/mascotas_pethome.csv
+
+connect sys/system as sysdba
+create or replace directory archivos_csv as '/tmp/archivos';
+grant read, write on directory archivos_csv to rol_admin;
+
+connect rj_proy_admin/rj_admin;
+
+set serveroutput on
+declare
+	v_return_value boolean;
+begin 
+	dbms_output.put_line('Entre');
+	v_return_value := exporta_mascota_csv('ARCHIVOS_CSV','mascotas_pethome.csv');
+	if v_return_value = true then
+		dbms_output.put_line('OK, se creo el archivo mascotas_pethome.csv');
+	else
+		raise_application_error(-20007,'ERROR: No se creo el archivo con los datos');
+	end if;
+end;
