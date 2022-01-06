058f42d7adc7da683323c0b52d5ac9784444a301
Hugo Juárez <aom921@hotmail.com>
2022-01-06 14:51:48 -0600

    Agrego fotos para LOB y cambios del web

--- Web/Webfinal/core/admin.py
+++ Web/Webfinal/core/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Mascota, Cliente 
+from .models import Mascota, Cliente, RevisionMascota, CentroOperativo
 # Register your models here.
 
 
 admin.site.register(Mascota)
 admin.site.register(Cliente)
+admin.site.register(RevisionMascota)
+admin.site.register(CentroOperativo)
